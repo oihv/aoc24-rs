165ed8359677a32cd8a6ac3aabedea43be9531d5
oihv <kokolas16@gmail.com>
2025-08-06 21:32:38 +0800

    Finish part 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,27 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("src/input.txt").unwrap();
+
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+
+    let mut lines = input.lines();
+
+    while let Some(line) = lines.next() {
+        let mut numbers = line.split("   ");
+
+        left.push(numbers.next().unwrap().parse().unwrap());
+        right.push(numbers.next().unwrap().parse().unwrap());
+    }
+
+    left.sort();
+    right.sort();
+
+    let mut sum = 0;
+    for i in 0..left.len() {
+        sum += (left[i] - right[i]).abs()
+    }
+
+    println!("{sum}");
 }
