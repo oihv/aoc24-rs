6e053b596a73ef038e0757d8f7879f397d9f8003
oihv <kokolas16@gmail.com>
2025-08-07 21:42:19 +0800

    Finish part two

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,10 +1,11 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn main() {
     let input = fs::read_to_string("src/input.txt").unwrap();
 
     let mut left: Vec<i32> = Vec::new();
     let mut right: Vec<i32> = Vec::new();
+    let mut right_map: HashMap<i32, i32> = HashMap::new();
 
     let mut lines = input.lines();
 
@@ -12,7 +13,9 @@ fn main() {
         let mut numbers = line.split("   ");
 
         left.push(numbers.next().unwrap().parse().unwrap());
-        right.push(numbers.next().unwrap().parse().unwrap());
+        let curr_right = numbers.next().unwrap().parse().unwrap();
+        right.push(curr_right);
+        right_map.entry(curr_right).and_modify(|count| *count += 1).or_insert(1);
     }
 
     left.sort();
@@ -23,5 +26,13 @@ fn main() {
         sum += (left[i] - right[i]).abs()
     }
 
-    println!("{sum}");
+    println!("Part one answer: {sum}");
+
+    // Part two, finding the similarity between left and right list
+    let mut similarity = 0;
+    for num in left {
+        similarity += num * right_map.get(&num).unwrap_or(&0);
+    }
+
+    println!("Part two answer: {similarity}");
 }
