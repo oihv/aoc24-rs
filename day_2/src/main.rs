befb610089a7eb641b59ee8fd04d4aeb2977f748
oihv <kokolas16@gmail.com>
2025-08-12 13:15:18 +0800

    Part 2 done

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -7,7 +7,7 @@ enum ReportTrend {
     Flat
 }
 
-fn get_report_trend(prev: i32, curr: i32) -> ReportTrend {
+fn get_report_trend(prev: &i32, curr: &i32) -> ReportTrend {
     if prev > curr {
         ReportTrend::Decreasing
     } else if curr > prev {
@@ -17,16 +17,16 @@ fn get_report_trend(prev: i32, curr: i32) -> ReportTrend {
     }
 }
 
-fn get_report_disparity(prev: i32, curr:i32) -> u32 {
-    prev.abs_diff(curr)
+fn get_report_disparity(prev: &i32, curr: &i32) -> u32 {
+    prev.abs_diff(*curr)
 }
 
-fn check_report_safety(report: String) -> bool {
-    let mut numbers = report.split(" ");
+fn check_report_safety(report: &Vec<i32>) -> bool {
+    let mut numbers = report.iter();
 
-    let mut prev = numbers.next().unwrap().parse().unwrap();
+    let mut prev = numbers.next().unwrap();
 
-    let curr = numbers.next().unwrap().parse().unwrap();
+    let curr = numbers.next().unwrap();
 
     let trend = get_report_trend(prev, curr);
 
@@ -35,20 +35,19 @@ fn check_report_safety(report: String) -> bool {
     }
 
     let disparity = get_report_disparity(prev, curr);
-    if disparity > 3 || disparity < 1 {
+    if !(1..=3).contains(&disparity) {
         return false;
     }
     
     prev = curr;
 
-    while let Some(curr) = numbers.next() {
-        let curr = curr.parse().unwrap();
+    for curr in numbers {
         if get_report_trend(prev, curr) != trend {
             return false;
         }  
 
         let disparity = get_report_disparity(prev, curr);
-        if disparity > 3 || disparity < 1 {
+        if !(1..=3).contains(&disparity) {
             return false;
         }
 
@@ -57,13 +56,27 @@ fn check_report_safety(report: String) -> bool {
     true
 }
 
-fn get_safe_report_sum(data: String) -> u32 {
-    let mut reports = data.lines();
+fn check_report_safety_fallback(nums: Vec<i32>) -> bool {
+    for i in 0..nums.len() {
+        let mut nums = nums.clone();
+        nums.remove(i);
 
+        if check_report_safety(&nums) {
+            return true
+        }
+    }
+    false
+    
+}
+
+fn get_safe_report_sum(data: String, fallback: bool) -> u32 {
+    let mut reports = data.lines();
     let mut sum = 0;
 
     while let Some(line) = reports.next() {
-        if check_report_safety(line.to_string()) {
+        let numbers = line.split(" ").map(|s| s.parse::<i32>().unwrap());
+        let numbers_vec: Vec<i32> = numbers.clone().collect();
+        if check_report_safety(&numbers_vec) || fallback && check_report_safety_fallback(numbers_vec) {
             sum += 1;
         }
     }
@@ -71,7 +84,11 @@ fn get_safe_report_sum(data: String) -> u32 {
 }
 
 fn main() {
-    println!("Testing\nPart 1: {}", get_safe_report_sum(fs::read_to_string("src/test.txt").expect("Cant't read the test file")));
+    println!("Testing\nPart 1: {}", get_safe_report_sum(fs::read_to_string("src/test.txt").expect("Cant't read the test file"), false));
 
-    println!("Real Input\nPart 1: {}", get_safe_report_sum(fs::read_to_string("src/input.txt").expect("Cant't read the input file")));
+    println!("Real Input\nPart 1: {}", get_safe_report_sum(fs::read_to_string("src/input.txt").expect("Cant't read the input file"), false));
+
+    println!("Testing\nPart 1: {}", get_safe_report_sum(fs::read_to_string("src/test.txt").expect("Cant't read the test file"), true));
+
+    println!("Real Input\nPart 1: {}", get_safe_report_sum(fs::read_to_string("src/input.txt").expect("Cant't read the input file"), true));
 }
